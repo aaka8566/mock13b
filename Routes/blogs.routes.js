324a5cc23918dcd352083722c32ee1c239a90779
Aakash Chakravarty <112715657+aaka8566@users.noreply.github.com>
2023-06-15 16:07:37 +0530

    mock13b

--- Routes/blogs.routes.js
+++ Routes/blogs.routes.js
@@ -26,4 +26,13 @@ blogsRouterd.delete("/delete/:id",async(req,res)=>{
        await blog.findByIdAndDelete({"_id":id});
    res.status(200).send("blog deleted successfully");
    });
+
+   blogsRouterd.patch("/edit/:id",async(req,res)=>{
+    //   console.log(req.body,"hi");
+    const {id}=req.params;
+
+       const blog=await blogModelm.findById({"_id":id});
+       await blog.findByIdAndUpdate({"_id":id},req.body);
+   res.status(200).send("blog updated successfully");
+   });  
 module.exports={blogsRouterd};
